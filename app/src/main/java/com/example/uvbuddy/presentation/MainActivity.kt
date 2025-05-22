0330ccd7cd32f65807d2e4473d061d7800a2a6ae
Hugo Tahara <taharatuto@gmail.com>
2025-05-21 22:17:37 -0300

    adding notifications and fix live data

--- app/src/main/java/com/example/uvbuddy/presentation/MainActivity.kt
+++ app/src/main/java/com/example/uvbuddy/presentation/MainActivity.kt
@@ -5,9 +5,18 @@
 
 package com.example.uvbuddy.presentation
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.example.uvbuddy.R
 import com.example.uvbuddy.databinding.ActivityMainBinding
 import com.example.uvbuddy.presentation.viewmodels.UVViewModel
 
@@ -22,6 +31,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        createNotificationChannel(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1001)
+            }
+        }
 
         // Observa LiveData e atualiza TextView
         viewModel.uvStatus.observe(this) { status ->
@@ -36,8 +51,42 @@ class MainActivity : AppCompatActivity() {
             binding.tvTip.text = tip
         }
 
+        viewModel.alertUv.observe(this){ status ->
+            status?.let { sendUVNotification(this, it) }
+        }
+
         // Chama requisição
-        viewModel.fetchUVLevel()
+        viewModel.restartScreen()
+    }
+
+    fun createNotificationChannel(context: Context) {
+        val name = "UV Alert"
+        val descriptionText = "Notificações de alerta UV"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel("uv_alerts", name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun sendUVNotification(context: Context, message: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+            ContextCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Sem permissão, retorna
+            return
+        }
+
+        val builder = NotificationCompat.Builder(context, "uv_alerts")
+            .setSmallIcon(R.drawable.ic_uv_alert)
+            .setContentTitle("Alerta de UV")
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        NotificationManagerCompat.from(context).notify(1001, builder.build())
     }
 }
 
--- app/src/main/java/com/example/uvbuddy/presentation/viewmodels/UVViewModel.kt
+++ app/src/main/java/com/example/uvbuddy/presentation/viewmodels/UVViewModel.kt
@@ -1,13 +1,23 @@
 package com.example.uvbuddy.presentation.viewmodels
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.uvbuddy.R
 import com.example.uvbuddy.presentation.repositories.UVRepository
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 
 import kotlinx.coroutines.launch
+
 
 class UVViewModel : ViewModel() {
     private val repo = UVRepository()
@@ -21,19 +31,26 @@ class UVViewModel : ViewModel() {
     private val _tip = MutableLiveData<String>("Carregando dica...")
     val tip: LiveData<String> = _tip
 
-    fun fetchUVLevel() {
+    private val _alertUv = MutableLiveData<String?>()
+    val alertUv: LiveData<String?> = _alertUv
+
+    private fun fetchUVLevel() {
         viewModelScope.launch {
             val uv = repo.fetchLatestUV()
             val (status, color) = when {
                 uv == null -> "Erro ao obter UV" to android.graphics.Color.GRAY
-                uv >= 8.0f -> "UV Muito Alto" to android.graphics.Color.RED
-                uv >= 5.0f -> "UV Alto" to android.graphics.Color.rgb(255, 165, 0) // Laranja
-                uv >= 2.0f -> "UV Moderado" to android.graphics.Color.YELLOW
-                uv >  0.0f -> "UV Baixo" to android.graphics.Color.GREEN
+                uv >= 8.0f -> "Índice UV $uv\n   Muito Alto" to android.graphics.Color.RED
+                uv >= 5.0f -> "Índice UV $uv\n        Alto" to android.graphics.Color.rgb(255, 165, 0) // Laranja
+                uv >= 2.0f -> "Índice UV $uv\n   Moderado" to android.graphics.Color.YELLOW
+                uv >  0.0f -> "Índice UV $uv\n       Baixo" to android.graphics.Color.GREEN
                 else -> "Sem radiação UV" to android.graphics.Color.GRAY
             }
             _uvStatus.value = status
             _backgroundColor.value = color
+
+            if (uv != null && uv >= 5.0f) {
+                _alertUv.value = status
+            }
 
              _tip.value = when {
                 uv == null -> null
@@ -45,4 +62,15 @@ class UVViewModel : ViewModel() {
             }
         }
     }
+
+    fun restartScreen() {
+        val time : Long = 1800000
+        viewModelScope.launch {
+            while (true) {
+                fetchUVLevel()
+                delay(time)
+            }
+        }
+    }
+
 }
