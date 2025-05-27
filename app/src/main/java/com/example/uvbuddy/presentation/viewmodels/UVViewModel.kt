49cfbffb7a682d94d60f4564c2fd58a1f79883eb
Felipe Konda-FEKONDA <fekonda@motorola.com>
2025-05-27 16:52:31 -0300

    arrumando string nivel UV

--- app/src/main/java/com/example/uvbuddy/presentation/viewmodels/UVViewModel.kt
+++ app/src/main/java/com/example/uvbuddy/presentation/viewmodels/UVViewModel.kt
@@ -1,30 +1,23 @@
 package com.example.uvbuddy.presentation.viewmodels
 
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
 import android.graphics.Color
-import android.os.Build
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.AbsoluteSizeSpan
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.uvbuddy.R
 import com.example.uvbuddy.presentation.repositories.UVRepository
 import kotlinx.coroutines.delay
-
 import kotlinx.coroutines.launch
-
+import kotlin.math.round
 
 class UVViewModel : ViewModel() {
     private val repo = UVRepository()
 
-    private val _uvStatus = MutableLiveData<String>("Carregando UV...")
-    val uvStatus: LiveData<String> = _uvStatus
+    private val _uvStatus = MutableLiveData<SpannableString>("Carregando UV...".toSpannable())
+    val uvStatus: LiveData<SpannableString> = _uvStatus
 
     private val _backgroundColor = MutableLiveData<Int>()
     val backgroundColor: LiveData<Int> = _backgroundColor
@@ -38,17 +31,32 @@ class UVViewModel : ViewModel() {
     private fun fetchUVLevel() {
         viewModelScope.launch {
             try {
-                val uv = repo.fetchLatestUV()
+                val uvResponse = repo.fetchLatestUV()
+                val uv = uvResponse?.let { round(it) }
                 val (status, color) = when {
                     uv == null -> "Erro ao obter UV" to Color.GRAY
-                    uv >= 8.0f -> "Índice UV $uv\n   Muito Alto" to Color.RED
-                    uv >= 5.0f -> "Índice UV $uv\n        Alto" to Color.rgb(255, 165, 0) // Laranja
-                    uv >= 2.0f -> "Índice UV $uv\n   Moderado" to Color.YELLOW
-                    uv >  0.0f -> "Índice UV $uv\n       Baixo" to Color.GREEN
+                    uv >= 8.0f -> "$uv\nMuito Alto" to Color.RED
+                    uv >= 5.0f -> "$uv\nAlto" to Color.rgb(255, 165, 0) // Laranja
+                    uv >= 2.0f -> "$uv\nModerado" to Color.YELLOW
+                    uv > 0.0f -> "$uv\nBaixo" to Color.GREEN
                     else -> "Sem radiação UV" to Color.GRAY
                 }
 
-                _uvStatus.value = status
+                // Criando o SpannableString para formatar o texto
+                val spannableStatus = status.toSpannable()
+
+                // Aplique a formatação apenas no valor UV
+                uv?.let {
+                    val uvStart = status.indexOf(it.toString())
+                    val uvEnd = uvStart + it.toString().length
+
+                    spannableStatus.setSpan(
+                        AbsoluteSizeSpan(40, true),  // Tamanho maior para o valor UV
+                        uvStart, uvEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                }
+
+                _uvStatus.value = spannableStatus
                 _backgroundColor.value = color
 
                 if (uv != null && uv >= 5.0f) {
@@ -60,12 +68,12 @@ class UVViewModel : ViewModel() {
                     uv >= 8.0f -> "Proteção máxima"
                     uv >= 5.0f -> "Use protetor solar"
                     uv >= 2.0f -> "Use óculos e boné"
-                    uv >  0.0f -> "Não se preocupe"
+                    uv > 0.0f -> "Não se preocupe"
                     else -> "Sem dica"
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
-                _uvStatus.value = "Erro de conexão"
+                _uvStatus.value = "Erro de conexão".toSpannable()
                 _backgroundColor.value = Color.GRAY
                 _tip.value = "Verifique sua conexão"
             }
@@ -73,7 +81,7 @@ class UVViewModel : ViewModel() {
     }
 
     fun restartScreen() {
-        val time : Long = 1800000
+        val time: Long = 1800000
         viewModelScope.launch {
             while (true) {
                 fetchUVLevel()
@@ -81,5 +89,8 @@ class UVViewModel : ViewModel() {
             }
         }
     }
+}
 
+fun String.toSpannable(): SpannableString {
+    return SpannableString(this)
 }
